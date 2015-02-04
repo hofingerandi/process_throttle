2a7545fcd8d25f3cf4d19c31159e4538eaca172c
hofingerandi <hofingerandi@gmx.at>
2015-02-04 15:17:15 +0000

    Initial checkin
    git-svn-id: https://svn.code.sf.net/p/processthrottle/code@2 dababbd7-7adc-48e6-9c65-0da661c0760b

---
+++ trunk/Gui.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace slowdown
+{
+    public partial class Gui : Form
+    {
+        SynchronizationContext theContext;
+        public Gui()
+        {
+            InitializeComponent();
+            theContext = SynchronizationContext.Current;
+            Thread.CurrentThread.Priority = ThreadPriority.Highest; // keep ui responsive
+
+            StartCpuTimer(); // monitor cpu%
+            StartTextboxTimer(); // refresh display
+
+            SuspendWorker = new SuspendWorker("processName");
+            UpdateWorker(0);
+
+            Blocker = new CpuBlocker();
+            Blocker.PropertyChanged += BlockerChanged;
+            UpdateBlocker();
+        }
+
+        CpuBlocker Blocker;
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            if (!e.Cancel && SuspendWorker != null)
+                SuspendWorker.Stop();
+        }
+
+        #region Measure Cpu %
+
+        System.Threading.Timer CpuTimer;
+        private void StartCpuTimer()
+        {
+            if (CpuTimer != null)
+                CpuTimer.Dispose();
+            CpuTimer = new System.Threading.Timer(ComputeCpu, null, 200, Timeout.Infinite);
+        }
+
+        double Cpu = 0;
+        DateTime lastTime;
+        double lastProcessorTime;
+        bool firstTime = true;
+        private void ComputeCpu(object o)
+        {
+            var currentProcTime = Process.GetCurrentProcess().TotalProcessorTime.TotalMilliseconds;
+            var currentTime = DateTime.Now;
+            var percentage = (currentProcTime - lastProcessorTime) / (double)((currentTime - lastTime).TotalMilliseconds);
+            percentage /= Environment.ProcessorCount;
+            lastProcessorTime = currentProcTime;
+            lastTime = currentTime;
+
+            if (firstTime)
+            {
+                firstTime = false;
+            }
+            else
+            {
+                lock (this)
+                {
+                    Cpu = percentage;
+                }
+            }
+            StartCpuTimer();
+        }
+
+        #endregion
+
+        #region Update Textboxes
+
+        System.Threading.Timer TextboxTimer;
+        private void StartTextboxTimer()
+        {
+            if (TextboxTimer != null)
+                TextboxTimer.Dispose();
+            TextboxTimer = new System.Threading.Timer(UpdateTextBox, null, 50, Timeout.Infinite);
+        }
+
+        private void UpdateTextBox(object o)
+        {
+            theContext.Post(
+                new SendOrPostCallback((ob) => UpdateText()), null);
+        }
+
+        public void UpdateText()
+        {
+
+            lock (this)
+            {
+                txtCpu.Text = String.Format("{0:F2}%", Cpu * 100);
+            }
+            var value = Interlocked.Read(ref Blocker.RunningThreadsCount);
+            txtThreadCount.Text = value.ToString();
+            if (SuspendWorker != null)
+                txtSuspendedProcess.Text = SuspendWorker.SuspenedProcess;
+
+            StartTextboxTimer();
+        }
+
+        #endregion
+
+        void BlockerChanged(object sender, PropertyChangedEventArgs e)
+        {
+            bool isRunning = Blocker.IsRunning;
+            numericUpDown1.Invoke(new Action(() => { numericUpDown1.Enabled = !isRunning; }));
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Blocker.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Blocker.Stop();
+        }
+
+        SuspendWorker SuspendWorker;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SuspendWorker != null)
+                SuspendWorker.Stop();
+
+            if (txtWorkerName.Text.Length > 0)
+                SuspendWorker = new SuspendWorker(txtWorkerName.Text);
+            else
+                SuspendWorker = new SuspendWorker("perfexp");
+
+            SuspendWorker.Start();
+            UpdateWorker(trackBar1.Value);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (SuspendWorker != null)
+                SuspendWorker.Stop();
+        }
+
+        private void UpdateWorker(int value)
+        {
+            value = Math.Max(0, Math.Min(100, value));
+            if (SuspendWorker != null)
+                SuspendWorker.Update(value, 100 - value);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblPercentage.Text = trackBar1.Value.ToString();
+            UpdateWorker(trackBar1.Value);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            UpdateBlocker();
+        }
+
+        private void UpdateBlocker()
+        {
+            Blocker.ThreadCount = (int)numericUpDown1.Value;
+        }
+    }
+}
---
+++ trunk/NativeMethods.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slowdown
+{
+    public static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+             ProcessAccessFlags processAccess,
+             bool bInheritHandle,
+             int processId
+        );
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess(flags, false, proc.Id);
+        }
+    }
+
+    public struct STARTUPINFO
+    {
+        public uint cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public uint dwX;
+        public uint dwY;
+        public uint dwXSize;
+        public uint dwYSize;
+        public uint dwXCountChars;
+        public uint dwYCountChars;
+        public uint dwFillAttribute;
+        public uint dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
+    }
+
+    public struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public uint dwProcessId;
+        public uint dwThreadId;
+    }
+
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        All = 0x001F0FFF,
+        Terminate = 0x00000001,
+        CreateThread = 0x00000002,
+        VirtualMemoryOperation = 0x00000008,
+        VirtualMemoryRead = 0x00000010,
+        VirtualMemoryWrite = 0x00000020,
+        DuplicateHandle = 0x00000040,
+        CreateProcess = 0x000000080,
+        SetQuota = 0x00000100,
+        SetInformation = 0x00000200,
+        QueryInformation = 0x00000400,
+        QueryLimitedInformation = 0x00001000,
+        Synchronize = 0x00100000
+    }
+
+    public static class ProcessExtension
+    {
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+
+        public static void Suspend(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                SuspendThread(pOpenThread);
+            }
+        }
+        public static void Resume(this Process process)
+        {
+            foreach (ProcessThread thread in process.Threads)
+            {
+                var pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    break;
+                }
+                ResumeThread(pOpenThread);
+            }
+        }
+    }
+}
